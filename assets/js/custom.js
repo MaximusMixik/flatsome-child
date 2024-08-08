e17835db84f1aaa41bf91d566a1f1e6a56e6a1c5
MaximusMixik <maximus.mixik@gmail.com>
2024-08-08 19:04:43 +0300

    moving scripts and header styles to the appropriate files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,56 @@
-jQuery(document).ready(function($){
-    $('.accordion-item .accordion-title').on('click',function(){
-        if($(window).width() > 1200) {
+jQuery(document).ready(function ($) {
+    $('.accordion-item .accordion-title').on('click', function () {
+        if ($(window).width() > 1200) {
             $('html, body').animate({
                 scrollTop: $('.product-page-accordian').offset().top - 150
             }, 1000);
         }
     })
+
+
+    // !!! header
+    // document.addEventListener('DOMContentLoaded', function () {
+    // Дождемся загрузки DOM, чтобы убедиться, что все элементы доступны
+
+    // Находим кнопку меню, элемент body, элемент #main и элемент .shop-link
+    var menuBtn = document.querySelector('.menuburger-btn');
+    var body = document.body;
+    var main = document.querySelector('#main');
+    var shopLink = document.querySelector('.shop-link');
+    var backMenuBtn = document.querySelector('.back-btn-menu');
+    var closeMenuBtn = document.querySelector('.close-menu-burger');
+
+
+    // Проверяем, что кнопка меню, body, элемент #main и .shop-link существуют
+    if (menuBtn && body && main && shopLink) {
+        // Добавляем обработчик события клика на кнопку меню
+        menuBtn.addEventListener('click', function () {
+            // Переключаем класс у body
+            body.classList.toggle('active-burger-menu');
+        });
+
+        // Добавляем обработчик события клика на элемент .shop-link
+        shopLink.addEventListener('click', function () {
+            // Переключаем класс у body
+            body.classList.toggle('active-burger-menu-category');
+        });
+
+        backMenuBtn.addEventListener('click', function () {
+            // Переключаем класс у body
+            body.classList.remove('active-burger-menu-category');
+        });
+
+        closeMenuBtn.addEventListener('click', function () {
+            // Переключаем класс у body
+            body.classList.remove('active-burger-menu');
+        });
+
+        // Добавляем обработчик события клика на элемент #main
+        main.addEventListener('click', function () {
+            body.classList.remove('active-burger-menu');
+            body.classList.remove('active-burger-menu-category');
+        });
+    }
+    // });
+
 });
--- functions.php
+++ functions.php
@@ -5,6 +5,8 @@
 
 function codelibry_name_scripts() {
 	wp_enqueue_style( 'codelibry-main-css', get_stylesheet_directory_uri() . '/assets/css/codelibry-main.css' );
+
+	wp_enqueue_script( 'codelibry-main-script', get_stylesheet_directory_uri() . '/assets/js/custom.js', array(), false );
 }
 add_action( 'wp_enqueue_scripts', 'codelibry_name_scripts' );
 
