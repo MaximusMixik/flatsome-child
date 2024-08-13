991b009872b91fece0ad45081520d84320f9fad4
MaximusMixik <maximus.mixik@gmail.com>
2024-08-13 19:13:30 +0300

    finish blog page

--- home.php
+++ home.php
@@ -10,8 +10,17 @@ get_header();
 
 do_action( 'flatsome_before_page' );?>
 
-<div id="content" class="blog-wrapper blog-archive page-wrapper">
-		<?php get_template_part( 'template-parts/posts/layout', get_theme_mod('blog_layout','right-sidebar') ); ?>
-</div>
+	<div class="blog__header">
+		<div class="container">
+			<?php echo do_shortcode('[rank_math_breadcrumb]'); ?>
+			<h1>
+				Blogposts
+				<!-- <?php the_title(); ?> -->
+			</h1>
+		</div>
+	</div>
+	<div id="content" class="blog-wrapper blog-archive page-wrapper">
+			<?php get_template_part( 'template-parts/posts/layout', get_theme_mod('blog_layout','right-sidebar') ); ?>
+	</div>
 
 <?php get_footer(); ?>
--- template-parts/posts/archive-3-col.php
+++ template-parts/posts/archive-3-col.php
@@ -15,24 +15,24 @@ if ( have_posts() ) : ?>
 		array_push($ids, get_the_ID());
 	endwhile; // end of the loop.
 	$ids = implode(',', $ids);
+
+
 ?>
-
-    <div class="custom-wrapper">
-
-	<?php
-	echo flatsome_apply_shortcode( 'blog_posts', array(
-		'type'        => get_theme_mod( 'blog_style_type', 'masonry' ),
-		'depth'       => get_theme_mod( 'blog_posts_depth', 0 ),
-		'depth_hover' => get_theme_mod( 'blog_posts_depth_hover', 0 ),
-		'text_align'  => get_theme_mod( 'blog_posts_title_align', 'center' ),
-		'columns'     => '4',
-		'show_date'   => get_theme_mod( 'blog_badge', 1 ) ? 'true' : 'false',
-		'ids'         => $ids,
-		'show_category' => 'true',
-	) );
-	?>
-
-</div>
+ 
+    <div class="archive-posts">
+			<?php
+			echo flatsome_apply_shortcode( 'blog_posts', array(
+				'type'        => get_theme_mod( 'blog_style_type', 'masonry' ),
+				'depth'       => get_theme_mod( 'blog_posts_depth', 0 ),
+				'depth_hover' => get_theme_mod( 'blog_posts_depth_hover', 0 ),
+				'text_align'  => get_theme_mod( 'blog_posts_title_align', 'center' ),
+				'columns'     => '4',
+				'show_date'   => get_theme_mod( 'blog_badge', 1 ) ? 'true' : 'false',
+				'ids'         => $ids,
+				'show_category' => 'true',
+			) );
+			?>
+		</div>
 
 <?php flatsome_posts_pagination(); ?>
 
