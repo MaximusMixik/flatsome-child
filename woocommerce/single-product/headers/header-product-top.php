bde5f3555a9928a385531fa104e6d86a6a261ef7
Vitalii Omelchenko <67092563+vitaliiomelchenko@users.noreply.github.com>
2024-08-14 16:15:41 +0300

    Add promo banner to single product page

---
+++ woocommerce/single-product/headers/header-product-top.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Header product top.
+ *
+ * @package          Flatsome/WooCommerce/Templates
+ * @flatsome-version 3.16.0
+ */
+
+?>
+<?php echo do_shortcode('[shop_promo_slider_top]');?>
+<div class="page-title shop-page-title product-page-title 11111">
+	<div class="page-title-inner flex-row medium-flex-wrap container">
+	  <div class="flex-col flex-grow medium-text-center">
+	  		<?php do_action('flatsome_product_title') ;?>
+	  </div>
+
+	   <div class="flex-col medium-text-center">
+		   	<?php do_action('flatsome_product_title_tools') ;?>
+	   </div>
+	</div>
+</div>
