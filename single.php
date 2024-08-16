7a6269c4a68dc27f1f5d258998a842b1078d8f5b
MaximusMixik <maximus.mixik@gmail.com>
2024-08-16 09:52:56 +0300

    update single-product, breadcrumbs

--- single.php
+++ single.php
@@ -10,7 +10,6 @@ get_header();
 do_action( 'flatsome_before_page' );
 
 ?>
-<?php echo do_shortcode('[shop_promo_slider_top]');?>
 <div id="content" class="blog-wrapper blog-single page-wrapper">
 	<?php get_template_part( 'template-parts/posts/layout', get_theme_mod('blog_post_layout','right-sidebar') ); ?>
 	<?php
